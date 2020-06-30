496d631ce3dc5216210a7c688d534b3c875c05a2
Sunit Trivedi <SunitNila@users.noreply.github.com>
2020-06-30 17:08:38 +0530

    Add files via upload

---
+++ SimpleChattyBot.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+//import java.util.Formatter;
+
+public class SimpleChattyBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
+    public static void main(String[] args) {
+        greet("Jarvis", "June 2020"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
+
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hi there! My name is " + assistantName + ".");
+        System.out.println("I was born in " + birthYear + ".");
+        System.out.println("Can I have your name, Please!");
+    }
+
+    static void remindName() {
+        String nameOfUser = scanner.nextLine();
+        System.out.format("What a great name you have, %s! %n", nameOfUser);
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.format("WOW! Your age is %d; and you know what? that's a good time to start programming! %n", age);
+    }
+
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        // write your code here
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+
+        while (true) {
+            int ans = scanner.nextInt();
+            if (ans == 2) {
+                break;
+            } else {
+                System.out.println("Please, try again.");
+            }
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+    }
+}
